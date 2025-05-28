8f51a28343935bd4a88a44fe61b05a3d92b0cd68
AlinShul <alina.shulginova@gmail.com>
2025-05-28 10:35:02 +0300

    fix admin manage command: change User -> CustomUser

--- site_app/management/commands/createdefaultadmin.py
+++ site_app/management/commands/createdefaultadmin.py
@@ -1,6 +1,6 @@
-from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
-from site_app.telegram_bot import bot
+
+from site_app.models import CustomUser
 
 
 class Command(BaseCommand):
@@ -9,7 +9,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         self.stdout.write("Creating default admin user")
         try:
-            admin_user = User.objects.create_superuser(
+            admin_user = CustomUser.objects.create_superuser(
                 'admin',
                 'admin@mysite.ru',
                 password='sS8zK5bG6sH8hB0p'
